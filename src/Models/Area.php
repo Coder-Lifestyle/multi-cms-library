ef963aa19c35a12432c038b9aaac2ce793012738
vlad <ortizCHZ@gmail.com>
2025-01-02 19:41:59 +0100

    add area

---
+++ src/Models/Area.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Area extends Model
+{
+    protected $fillable = ['name', 'menu_id'];
+
+    /**
+     * Relationship to the Menu model.
+     * Each area is associated with one menu.
+     */
+    public function menu()
+    {
+        return $this->belongsTo(Menu::class);
+    }
+}
