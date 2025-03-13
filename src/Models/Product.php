d4199ac5eb17c9e3e0d4387a0f6c06c9d9f7772b
vlad <ortizCHZ@gmail.com>
2025-03-13 21:06:58 +0100

    add product

---
+++ src/Models/Product.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace MultiCmsLibrary\SharedModels\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'short_description',
+        'description',
+        'price',
+        'sale_price',
+        'domain_id',
+        'category_id',
+        'image_url',
+        'brand_name',
+        'stock_quantity',
+        'rating'
+    ];
+
+    /**
+     * Get the domain that owns the product.
+     */
+    public function domain()
+    {
+        return $this->belongsTo(Domain::class);
+    }
+
+    /**
+     * Get the category that owns the product.
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
